0856a93b5fff358345e65c9f55089cfc1cb69dbc
pranavphale8 <pranavmfale2001@gmail.com>
2023-09-06 14:08:39 +0530

    my portfolio

---
+++ portfolio.github.io/portfolio/script.js
@@ -0,0 +1,56 @@
+
+
+// scroll to top functionality
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+// Nav hamburgerburger selections
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+  });
+
+  // Close hamburger menu when a link is clicked
+
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
+
+// -------------contact sheet-----------------------
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzoalhVBrDZOnrkeaLrWxeqo_MsuDtpJR9M6PBU8BIJxWv6ZECFm_a4Hi2zjYats6Jt3A/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message sent successfully"
+            setTimeout(function(){
+                msg.innerHTML = ""
+            },5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
+
+
+
+
